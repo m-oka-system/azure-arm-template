8581e19187c0efceb5c1c4a5a08ca349f266cf5f
shirokuman <fun-for-fan@outlook.com>
2018-10-07 22:43:03 +0900

    first commit

---
+++ Get-AzureRmResourceProvider.ps1
@@ -0,0 +1,8 @@
+﻿# List up resource type
+Get-AzureRmResourceProvider | `
+    Select-Object ProviderNameSpace,ResourceTypes | `
+    Sort-Object ProviderNameSpace
+
+# Check the API version
+((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Compute).ResourceTypes | `
+    Where-Object {$_.ResourceTypeName -eq "virtualMachines"}).ApiVersions
---
+++ New-AzureRmResourceGroupDeployment.ps1
@@ -0,0 +1,37 @@
+﻿# Variable
+$resourceGroupName = "w-arm-rg"
+$location = "Japan West"
+$deploymentName = "SampleDeploy"
+$templatePath = "C:\OneDrive\DS218j\cloud\azure\azure-arm-template\app-service-with-traffice-manager\"
+$templateFile = "azuredeploy.json"
+$templateParameterFile = "azuredeploy.parameters.json"
+$template = $templatePath + $templateFile
+$templateParameter = $templatePath + $templateParameterFile
+
+# Create resource group
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location -Verbose -Force
+
+# Test template deployment
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -TemplateParameterFile $templateParameter -Verbose
+
+# Adds an Azure deployment to a resource group
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    -Name $deploymentName `
+    -TemplateFile $template `
+    -TemplateParameterFile $templateParameter -Verbose -Force
+
+
+# Show History
+Get-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName
+
+# Delete all resources
+$deploymentName = "AllDelete"
+$templatePath = "C:\OneDrive\DS218j\cloud\azure\azure-arm-template\"
+$templateFile = "azuredeploy.json"
+$template = $templatePath + $templateFile
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    -Name $deploymentName `
+    -TemplateFile $template -Verbose -Force `
+    -Mode Complete
